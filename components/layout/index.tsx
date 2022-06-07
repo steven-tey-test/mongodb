bdf23a58c544727a01f3f6cf9390a4a4afd6eaea
steven-tey-test <prosniper942@gmail.com>
2022-06-07 15:49:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/layout/index.tsx
@@ -0,0 +1,46 @@
+import { useState, ReactNode } from 'react';
+import Sidebar from './sidebar';
+import Navbar from './navbar';
+import Directory from './directory';
+import { ResultProps } from '@/lib/api/user';
+import Meta, { MetaProps } from './meta';
+
+export default function Layout({
+  meta,
+  results,
+  totalUsers,
+  children
+}: {
+  meta: MetaProps;
+  results: ResultProps[];
+  totalUsers: number;
+  children: ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="w-full mx-auto h-screen flex overflow-hidden bg-black">
+      <Meta props={meta} />
+      <Sidebar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        results={results}
+        totalUsers={totalUsers}
+      />
+
+      <div className="flex flex-col min-w-0 flex-1 overflow-hidden">
+        <div className="flex-1 relative z-0 flex overflow-hidden">
+          <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last">
+            {/* Navbar */}
+            <Navbar setSidebarOpen={setSidebarOpen} />
+
+            {children}
+          </main>
+          <div className="hidden md:order-first h-screen md:flex md:flex-col">
+            <Directory results={results} totalUsers={totalUsers} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,49 @@
+import { GetStaticProps } from 'next';
+import Layout from '@/components/layout';
+import Profile from '@/components/profile';
+import {
+  getAllUsers,
+  ResultProps,
+  UserProps,
+  getUserCount,
+  getFirstUser
+} from '@/lib/api/user';
+
+export default function Home({
+  results,
+  totalUsers,
+  user
+}: {
+  results: ResultProps[];
+  totalUsers: number;
+  user: UserProps;
+}) {
+  const ogUrl = 'https://mongodb.vercel.app';
+  const meta = {
+    title: 'MongoDB Starter Kit',
+    description:
+      'MongoDB Starter Kit built with Next.js, Vercel, and MongoDB Atlas.',
+    ogImage: `https://mongodb.vercel.app/og.png`,
+    ogUrl
+  };
+  return (
+    <Layout meta={meta} results={results} totalUsers={totalUsers}>
+      <Profile user={user} settings={false} />
+    </Layout>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const results = await getAllUsers();
+  const totalUsers = await getUserCount();
+  const firstUser = await getFirstUser();
+
+  return {
+    props: {
+      results,
+      totalUsers,
+      user: firstUser
+    },
+    revalidate: 60
+  };
+};
